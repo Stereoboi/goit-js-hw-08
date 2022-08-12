b948c2f8a09e5e424d254b6d0e5f43637cf9461e
Bohdan Pishchak <3.14shcha@gmail.com>
2022-08-12 10:12:51 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+// ----------------------------Формуємо розмітку-------------------------------------------
+const getGalleryRef = document.querySelector(".gallery");
+const addGalleryPictures = galleryItems.map(({ preview, original, description }) => {
+    console.log(description)
+    return `<a class="gallery__item" href=${original}>
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>`;
+}).join("");
+
+
+// ----------------------------Рендеримо розмітку-------------------------------------------
+
+getGalleryRef.innerHTML = addGalleryPictures;
+
+
+// ----------------------------Скидаємо всю роботу на simplelightbox -------------------------------------------
+new SimpleLightbox('.gallery a', {
+    scrollZoom: true,
+    captions: true,
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,2 @@
-
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
