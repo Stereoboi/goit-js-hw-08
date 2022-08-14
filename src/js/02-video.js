28c899abe27deaf99a740dd588567c3b01f557cb
Bohdan Pishchak <3.14shcha@gmail.com>
2022-08-14 18:58:08 +0300

    2-3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,19 @@
+
 import Player from '@vimeo/player';
-let throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(pausedTime, 1000));
+
+function pausedTime(evt) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(evt.seconds)
+  );
+}
+
+const time = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+player.setCurrentTime(time);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
+const formRef = document.querySelector('form');
+const STORAGE_KEY = 'feedback-form-state';
+populateTextInput()
 
+formRef.addEventListener('submit', getChangedFormValue);
+formRef.addEventListener('input', throttle(enteredValues, 500));
+
+function getChangedFormValue(event) {
+    event.preventDefault();
+
+    const { elements } = event.target;
+    const values = {};
+
+    for (let i = 0; i < elements.length; i += 1){
+
+        const formElement = elements[i];
+        const {name} = formElement;
+        
+        if (name) {
+        const { value } = formElement;
+        values[name] = value;
+        }
+    }
+    console.log('Submited', values);
+    localStorage.removeItem(STORAGE_KEY);
+    event.currentTarget.reset()
+}
+
+function enteredValues() {
+
+    const formData = new FormData(formRef);
+    const values = Object.fromEntries(formData.entries());
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(values));
+    
+}
+
+
+function populateTextInput() {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedData) {
+        formRef.message.value = savedData.message;
+        formRef.email.value = savedData.email;
+    }
+}
